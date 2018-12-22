263ab8b81b51a4f5c80b7f57ab6e72c0f45fede3
Willian van der Velde <mail@willian.io>
2018-12-22 09:30:12 +0100

    (wip) To Arel and back (#13)
    * To Arel and back
    
    * Oops
    
    * More Query
    
    * More Query
    
    * Nodes
    
    * Different strategy
    
    * visitor pattern
    
    * stuff
    
    * SubQuery
    
    * Joins

--- lib/to_arel.rb
+++ lib/to_arel.rb
@@ -2,27 +2,136 @@ require 'to_arel/version'
 require 'arel'
 require 'pg_query'
 
+def symbolize_keys(hash)
+  Hash[hash.map{|k, v| [k.to_sym, v]}]
+end
+
 #:nodoc:
 module ToArel
+  class UnboundColumnReference < ::Arel::Nodes::SqlLiteral; end
+
+  class Visitor
+    def accept(object)
+      visit *klass_and_attributes(object)
+    end
+
+    private
+
+    def visit(klass, attributes)
+      dispatch_method = "visit_#{klass}"
+      puts "Visiting #{dispatch_method}"
+      send dispatch_method, klass, attributes
+    end
+
+    def klass_and_attributes(object)
+      [object.keys.first, object.values.first]
+    end
+
+    private
+
+    def visit_String(klass, attributes)
+      attributes['str']
+    end
+
+    def visit_ColumnRef(klass, attributes)
+      UnboundColumnReference.new(
+        attributes['fields'].map do |field|
+          visit *klass_and_attributes(field)
+        end.join('.') # TODO: Join . might be a poor assumption
+      )
+    end
+
+    def visit_ResTarget(klass, attributes)
+      visit *klass_and_attributes(attributes['val'])
+    end
+
+    def visit_SubLink(klass, attributes)
+      puts klass
+      puts attributes
+      puts 'PLZ IMPLEMENT SUB LINK'
+    end
+
+    def visit_RangeVar(klass, attributes)
+      # TODO: I'm not sure if RangeVar should already create a Table
+      Arel::Table.new attributes['relname']
+    end
+
+    def visit_A_Expr(klass, attributes)
+      puts 'IMPLEMENT ME'
+      puts klass
+      puts attributes
+
+    end
+
+    def visit_JoinExpr(klass, attributes)
+      # case node['jointype']
+      # when 0
+      #   if node['isNatural']
+      #     output << 'NATURAL'
+      #   elsif node['quals'].nil? && node['usingClause'].nil?
+      #     output << 'CROSS'
+      #   end
+      # when 1
+      #   output << 'LEFT'
+      # when 2
+      #   output << 'FULL'
+      # when 3
+      #   output << 'RIGHT'
+      # end
+      larg = visit(*klass_and_attributes(attributes['larg']))
+      rarg = visit(*klass_and_attributes(attributes['rarg']))
+
+      quals = visit(*klass_and_attributes(attributes['quals']))
+
+      puts 'MERGE EVERYTHNIG TOGHETER'
+
+      Arel::Nodes::OuterJoin.new larg, rarg
+    end
+
+    def visit_FuncCall(klass, attributes)
+      args = attributes['args'].map { |arg| visit(*klass_and_attributes(arg)) }
+
+      # TODO: Everything is a count :)
+      Arel::Nodes::Count.new args
+    end
+
+    def visit_SelectStmt(klass, attributes)
+      froms = if (from_clauses = attributes['fromClause'])
+        from_clauses.map { |from_clause| visit *klass_and_attributes(from_clause) }
+      end
+
+      targets = if (target_list = attributes['targetList'])
+        target_list.map { |target| visit *klass_and_attributes(target) }
+      end
+
+      from, *joins = froms
+
+      select_manager = Arel::SelectManager.new(from) # TODO: multi-from is goneraz
+      select_manager.projections = targets
+      select_manager
+    end
+
+    def visit_RawStmt(klass, attributes)
+      if (stmt = attributes['stmt'])
+        visit *klass_and_attributes(stmt)
+      end
+    end
+  end
+
   class Error < StandardError; end
+  class MultiTableError < Error; end
 
   def self.parse(sql)
     tree = PgQuery.parse(sql).tree
+    puts tree
 
-    raise 'cannot process more than 1 statement' if tree.length > 1
-
-    statement = tree.first
-      .fetch('RawStmt')
-      .fetch('stmt')
-
-    raise 'dunno how to handle more than 1 statement' if statement.keys.length > 1
-
-    manager_from_statement statement
+    Visitor.new.accept(tree.first) # DUNNO Y .first
   end
 
   def self.manager_from_statement(statement)
     type = statement.keys.first
     ast = statement[type]
+
 
     case type
     when 'SelectStmt'
@@ -33,13 +142,20 @@ module ToArel
   end
 
   def self.create_select_manager(ast)
-    table = ast.fetch('fromClause')
-    raise 'dunno how to handle multiple tables' if table.length > 1
 
-    table = table.first
+    # from_clauses = ast.fetch('fromClause')
+    # raise MultiTableError, 'Can not handle multiple tables' if from_clauses.length > 1
+    # from_clause = from_clauses.first
+    # table = RangeVar.new(**symbolize_keys(from_clause.fetch('RangeVar'))).to_arel
 
-    table = Arel::Table.new(table.fetch('RangeVar').fetch('relname'))
+    # puts ast
 
-    Arel::SelectManager.new table
+    # target_lists = ast.fetch('targetList').map do |target_list|
+    #   ResTarget.new(**symbolize_keys(target_list.fetch('ResTarget'))).to_arel
+    # end
+
+    # a = Arel::SelectManager.new table
+    # b = a.project(target_lists)
+    # binding.pry
   end
 end
--- spec/to_arel_spec.rb
+++ spec/to_arel_spec.rb
@@ -1,3 +1,6 @@
+require 'support/fake_record'
+Arel::Table.engine = FakeRecord::Base.new
+
 RSpec.describe ToArel do
   it 'has a version number' do
     expect(ToArel::VERSION).not_to be nil
@@ -5,21 +8,51 @@ RSpec.describe ToArel do
 
   describe '.parse' do
     describe 'SELECT' do
-      it 'returns an arel select manager' do
-        expect(ToArel.parse('SELECT 1 FROM posts').class).to eq Arel::SelectManager
-      end
+      # it 'returns an arel select manager' do
+      #   expect(ToArel.parse('SELECT 1 FROM posts').class).to eq Arel::SelectManager
+      # end
 
-      it 'has the correct table set' do
-        expect(ToArel.parse('SELECT 1 FROM posts').froms).to eq [
-          Arel::Table.new('posts')
-        ]
+      # it 'has the correct table set' do
+      #   expect(ToArel.parse('SELECT 1 FROM posts').froms).to eq [
+      #     Arel::Table.new('posts')
+      #   ]
+      # end
+
+      describe 'to arel and back' do
+        it 'parses a simple query' do
+          given_sql = 'SELECT id FROM users'
+          expected_sql = 'SELECT id FROM "users"'
+
+          expect(ToArel.parse(given_sql).to_sql).to eq expected_sql
+        end
+
+        it 'parses a query with an aggregate' do
+          given_sql = 'SELECT count(id) FROM users'
+          expected_sql = 'SELECT COUNT(id) FROM "users"'
+
+          expect(ToArel.parse(given_sql).to_sql).to eq expected_sql
+        end
+
+        it 'parses a query with a subquery' do
+          given_sql = 'SELECT id, (SELECT id FROM users LIMIT 1) FROM photos'
+          expected_sql = 'SELECT id FROM "photos" INNER JOIN "users" ON "photos"."user_id" = "users"."id"'
+
+          expect(ToArel.parse(given_sql).to_sql).to eq expected_sql
+        end
+
+        it 'parses a query with an aggrgate' do
+          given_sql = 'SELECT id FROM photos INNER JOIN users ON photos.user_id = users.id'
+          expected_sql = 'SELECT id FROM "photos" INNER JOIN "users" ON "photos"."user_id" = "users"."id"'
+
+          expect(ToArel.parse(given_sql).to_sql).to eq expected_sql
+        end
       end
     end
 
-    describe 'UPDATE' do
+    xdescribe 'UPDATE' do
     end
 
-    describe 'INSERT' do
+    xdescribe 'INSERT' do
     end
   end
 end
