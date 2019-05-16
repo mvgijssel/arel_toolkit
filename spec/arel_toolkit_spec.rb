0eb9eab81ca3847e76c258f9536ca79b38671b68
maarten <maarten@vgijssel.nl>
2019-05-16 22:00:23 +0200

    Setup rspec

--- spec/arel_toolkit_spec.rb
+++ spec/arel_toolkit_spec.rb
@@ -1,9 +1,5 @@
-RSpec.describe ArelToolkit do
+describe ArelToolkit do
   it "has a version number" do
     expect(ArelToolkit::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
