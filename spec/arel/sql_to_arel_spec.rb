3f47671581b2657722794a7cdbaeb6c3c3c1d668
maarten <maarten@vgijssel.nl>
2019-05-22 21:58:55 +0200

    WIP PgQuery::DECLARE_CURSOR_STMT

--- spec/arel/sql_to_arel_spec.rb
+++ spec/arel/sql_to_arel_spec.rb
@@ -85,6 +85,7 @@ describe 'Arel.sql_to_arel' do
   # visit 'sql', "CREATE TABLE a AS (SELECT * FROM reports)", 'PgQuery::CREATE_TABLE_AS_STMT'
   # visit 'sql', "CREATE TRIGGER a AFTER INSERT ON b FOR EACH ROW EXECUTE PROCEDURE b()", 'PgQuery::CREATE_TRIG_STMT'
   # visit 'sql', "DEALLOCATE some_prepared_statement", 'PgQuery::DEALLOCATE_STMT'
+  # visit 'sql', "DECLARE a CURSOR FOR SELECT 1", 'PgQuery::DECLARE_CURSOR_STMT'
 
   # # NOTE: should run at the end
   # children.each do |child|
