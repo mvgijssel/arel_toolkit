a24677bcbbc91b23f016bfdf3246308b337f0616
maarten <maarten@vgijssel.nl>
2019-05-25 23:30:33 +0200

    PgQuery::UPDATE_STMT

--- spec/arel/sql_to_arel_spec.rb
+++ spec/arel/sql_to_arel_spec.rb
@@ -187,6 +187,7 @@ describe 'Arel.sql_to_arel' do
   visit 'pg', 'TRUNCATE public.some_table', 'PgQuery::TRUNCATE_STMT'
   visit 'all', "SELECT 1::int4, 2::bool, '3'::text", 'PgQuery::TYPE_CAST'
   visit 'all', 'SELECT "a"::varchar', 'PgQuery::TYPE_NAME'
+  visit 'pg', 'UPDATE some_table SET some_column = 1', 'PgQuery::UPDATE_STMT'
 
   it 'translates FETCH into LIMIT' do
     sql = 'SELECT 1 FETCH FIRST 2 ROWS ONLY'
