c490b7dad5c0a00248f5c1722d4e31f570c16bd7
maarten <maarten@vgijssel.nl>
2019-06-20 02:28:11 +0200

    Ignore spec files for coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 require 'simplecov'
 require 'simplecov-console'
 SimpleCov.formatter = SimpleCov::Formatter::Console
-SimpleCov.start
+SimpleCov.start do
+  add_filter %r{^/spec/}
+end
 
 require 'bundler/setup'
 require 'pry'
