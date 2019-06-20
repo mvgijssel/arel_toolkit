0cb608308a577706e4566fc67a7cd1131c6bd61f
maarten <maarten@vgijssel.nl>
2019-06-20 05:25:31 +0200

    Fix superclass mismatch for postgres_ext

--- lib/arel/extensions/overlap.rb
+++ lib/arel/extensions/overlap.rb
@@ -1,10 +1,24 @@
+# rubocop:disable Naming/MethodName
+# rubocop:disable Naming/UncommunicativeMethodParamName
+
 module Arel
   module Nodes
     # https://www.postgresql.org/docs/9.1/functions-array.html
-    class Overlap < InfixOperation
-      def initialize(left, right)
-        super(:'&&', left, right)
+    class Overlap < Arel::Nodes::Binary
+      def operator
+        '&&'
+      end
+    end
+  end
+
+  module Visitors
+    class ToSql
+      def visit_Arel_Nodes_Overlap(o, collector)
+        infix_value o, collector, ' && '
       end
     end
   end
 end
+
+# rubocop:enable Naming/MethodName
+# rubocop:enable Naming/UncommunicativeMethodParamName
