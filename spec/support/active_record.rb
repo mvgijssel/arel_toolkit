ec9d5aab0b518eaa250d316d5fe406162cb99b3f
Willian van der Velde <willian@hackerone.com>
2019-07-18 11:17:37 +0200

    Balanced AST

--- spec/support/active_record.rb
+++ spec/support/active_record.rb
@@ -24,9 +24,9 @@ def cast_for_database(value)
   when Integer
     value
   when TrueClass
-    Arel::Nodes::TypeCast.new(Arel.sql('\'t\''), 'bool')
+    Arel::Nodes::TypeCast.new(Arel::Nodes::Quoted.new('t'), 'bool')
   when FalseClass
-    Arel::Nodes::TypeCast.new(Arel.sql('\'f\''), 'bool')
+    Arel::Nodes::TypeCast.new(Arel::Nodes::Quoted.new('f'), 'bool')
   when Float
     value
   else
