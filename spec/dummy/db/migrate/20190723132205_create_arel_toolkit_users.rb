04f3805dd1411d130b51a41349b377c3cf266952
Willian van der Velde <willian@hackerone.com>
2019-07-23 15:45:41 +0200

    Add dummy app

---
+++ spec/dummy/db/migrate/20190723132205_create_arel_toolkit_users.rb
@@ -0,0 +1,9 @@
+class CreateArelToolkitUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+
+      t.timestamps
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../dummy/config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
@@ -24,6 +24,7 @@ require 'rspec/rails'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
