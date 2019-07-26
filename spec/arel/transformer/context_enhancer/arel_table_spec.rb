0a1e052b9076fde0897e30ba923bb3097e058b64
maarten <maarten@vgijssel.nl>
2019-07-26 23:16:55 +0200

    Moved the context enhancer spec

---
+++ spec/arel/transformer/context_enhancer/arel_table_spec.rb
@@ -0,0 +1,13 @@
+describe Arel::Transformer::ContextEnhancer::ArelTable do
+  it 'adds additional context to Arel::Table nodes' do
+    result = Arel.sql_to_arel('SELECT posts.id FROM posts')
+    original_arel = result.first
+
+    tree = Arel.transformer(original_arel)
+    from_table_node = tree.child_at_path(['ast', 'cores', 0, 'source', 'left'])
+    projection_table_node = tree.child_at_path(['ast', 'cores', 0, 'projections', 0, 'relation'])
+
+    expect(from_table_node.context).to eq(range_variable: true, column_reference: false)
+    expect(projection_table_node.context).to eq(range_variable: false, column_reference: true)
+  end
+end
--- spec/arel/transformer_spec.rb
+++ spec/arel/transformer_spec.rb
@@ -154,16 +154,4 @@ describe 'Arel.transformer' do
 
     expect(original_arel).to be_identical_arel(new_arel)
   end
-
-  it 'adds additional context to Arel::Table nodes' do
-    result = Arel.sql_to_arel('SELECT posts.id FROM posts')
-    original_arel = result.first
-
-    tree = Arel.transformer(original_arel)
-    from_table_node = tree.child_at_path(['ast', 'cores', 0, 'source', 'left'])
-    projection_table_node = tree.child_at_path(['ast', 'cores', 0, 'projections', 0, 'relation'])
-
-    expect(from_table_node.context).to eq(range_variable: true, column_reference: false)
-    expect(projection_table_node.context).to eq(range_variable: false, column_reference: true)
-  end
 end
