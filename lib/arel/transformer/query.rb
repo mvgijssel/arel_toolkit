6c0b4f9d48e3c6dd85252acdec6850c02b59800f
maarten <maarten@vgijssel.nl>
2019-07-30 22:04:04 +0200

    Remove comments

--- lib/arel/transformer/query.rb
+++ lib/arel/transformer/query.rb
@@ -2,24 +2,9 @@ module Arel
   module Transformer
     class Query
       def self.call(node, kwargs)
-        node_attributes = [:context, :parent]
+        node_attributes = %i[context parent]
         node_args = kwargs.slice(*node_attributes)
         object_args = kwargs.except(*node_attributes)
-
-        # TODO:
-        # parent refers to a Node, so the matcher probably needs to be a hash:
-        # t.query(parent: { class: Arel::Table })
-
-        # query(context: { foo: :bar })
-        # should match a node with a context of { foo: :bar, papi: :chulo }
-        # we're matching Hash against Hash, so in that case choose the "includes" strategy?
-
-        # query(parent: Arel::Table)
-        # which is short hand for
-        # query(parent: { class: { Arel::Table } })
-        # and also short hand for
-        # query(parent: { object: { class: { Arel::Table } } } })
-        # query(parent: { context: { range_variable: true } })
 
         node.each.select do |child_node|
           next unless matches?(child_node, node_args)
@@ -28,17 +13,9 @@ module Arel
         end
       end
 
-      private
-
-      # { context: { kerk: :shine } }
-      # Node.context <kerk => shine>
       def self.matches?(object, test)
-        # If test value is a Hash, maybe go recursive.
-        # all else test values are direct compare?
         case test
         when Hash
-          # When the object is a Hash, compare hashes using subset
-          # otherwise check if all members exist on object
           case object
           when Hash
             test <= object
