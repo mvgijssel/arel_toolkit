08dfcdd88c2a7fe11de10759c4088e96ee89a93e
maarten <maarten@vgijssel.nl>
2019-08-03 14:57:31 +0200

    Added support for multiple schemas, priority and regclass

--- lib/arel/transformer/prefix_schema_name.rb
+++ lib/arel/transformer/prefix_schema_name.rb
@@ -1,25 +1,114 @@
 module Arel
   module Transformer
     class PrefixSchemaName
-      attr_reader :schema_name
+      PG_CATALOG = 'pg_catalog'.freeze
+      DEFAULT_SCHEMA_PRIORITY = ['public', PG_CATALOG].freeze
 
-      def initialize(schema_name)
-        @schema_name = schema_name
+      attr_reader :connection
+      attr_reader :table_mapping
+      attr_reader :schema_priority
+
+      def initialize(
+        connection,
+        schema_priority = DEFAULT_SCHEMA_PRIORITY,
+        override_table_mapping = {}
+      )
+        @connection = connection
+        @schema_priority = schema_priority
+        @table_mapping = database_table_mapping.merge(override_table_mapping)
       end
+
+      # We don't need to prefix nodes with `pg_catalog`, because that's the default
+      # even if the search_path does not include `pg_catalog`. Same for `information_schema`
 
       # https://github.com/mvgijssel/arel_toolkit/issues/110
       def call(arel, _context)
         tree = Arel.enhance(arel)
+        update_tables(tree)
+        update_typecasts(tree)
+        tree.object
+      end
 
+      private
+
+      def update_tables(tree)
         tree.query(
           class: Arel::Table,
           schema_name: nil,
           context: { range_variable: true },
         ).each do |node|
+          schema_name = schema_name_from_table_name(node['name'].object.to_s)
           node['schema_name'].replace(schema_name)
         end
+      end
 
-        tree.object
+      def update_typecasts(tree)
+        tree.query(
+          class: Arel::Nodes::TypeCast,
+          type_name: 'regclass',
+        ).each do |node|
+          update_typecast_node(node)
+        end
+      end
+
+      def update_typecast_node(node)
+        table_name = table_name_from_arel_node(node['arg'].object)
+        reference_parts = table_name.split('.')
+
+        case reference_parts.length
+        when 1
+          schema_name = schema_name_from_table_name(table_name)
+          reference_parts.unshift(schema_name)
+          node['arg']['expr'].replace(reference_parts.join('.'))
+        when 2
+          node # Do nothing
+        else
+          raise "Don't know how to handle `#{reference_parts.length}` parts in " \
+                "`#{reference_parts}` for sql `#{node.to_sql}`"
+        end
+      end
+
+      def table_name_from_arel_node(arel_node)
+        case arel_node
+        when Arel::Nodes::Quoted
+          arel_node.expr
+        else
+          raise "Unknown node `#{table_name}` for `#{node.inspect}`"
+        end
+      end
+
+      def schema_name_from_table_name(table_name)
+        possible_schemas = table_mapping[table_name]
+
+        if possible_schemas.empty?
+          raise "Table `#{table_name}` is an unknown table and cannot be prefixed"
+        end
+
+        schema_name = schema_priority.find do |possible_schema_name|
+          possible_schemas.include?(possible_schema_name)
+        end
+
+        if schema_name.nil?
+          raise "Could not find a schema name for table `#{table_name}`.\n" \
+                "Current schema priority is `#{schema_priority}`.\n" \
+                "Possible schemas are `#{possible_schemas}`."
+        end
+
+        return nil if schema_name == PG_CATALOG
+
+        schema_name
+      end
+
+      def database_table_mapping
+        mapping = Hash.new { |m, k| m[k] = [] }
+
+        connection
+          .execute('SELECT tablename, schemaname FROM pg_catalog.pg_tables')
+          .each do |result|
+          mapping[result.fetch('tablename').to_s] << result.fetch('schemaname').to_s
+        end
+
+        mapping
       end
     end
   end
---
+++ spec/arel/transformer/prefix_schema_name_spec.rb
@@ -0,0 +1,101 @@
+describe Arel::Transformer::PrefixSchemaName do
+  let(:connection) { ActiveRecord::Base.connection }
+
+  context 'table' do
+    it 'adds a schema to a table' do
+      transformer = Arel::Transformer::PrefixSchemaName.new(connection)
+      sql = 'SELECT "posts"."id" FROM "posts"'
+      arel = Arel.sql_to_arel(sql)
+      prefixed_sql = transformer.call(arel.first, nil).to_sql
+
+      expect(prefixed_sql).to eq 'SELECT "posts"."id" FROM "public"."posts"'
+    end
+
+    it 'does not add a schema when a schema is already defined' do
+      transformer = Arel::Transformer::PrefixSchemaName.new(connection)
+      sql = 'SELECT "posts"."id" FROM "some_schema"."posts"'
+      arel = Arel.sql_to_arel(sql)
+      prefixed_sql = transformer.call(arel.first, nil).to_sql
+
+      expect(prefixed_sql).to eq 'SELECT "posts"."id" FROM "some_schema"."posts"'
+    end
+
+    it 'allows to override the default schema' do
+      transformer = Arel::Transformer::PrefixSchemaName.new(
+        connection,
+        %w[secret public],
+        'posts' => ['secret'],
+      )
+      sql = 'SELECT "posts"."id" FROM "posts" INNER JOIN "comments" ON TRUE'
+      arel = Arel.sql_to_arel(sql)
+      prefixed_sql = transformer.call(arel.first, nil).to_sql
+
+      expect(prefixed_sql).to eq(
+        'SELECT "posts"."id" FROM "secret"."posts" INNER JOIN "public"."comments" ON \'t\'::bool',
+      )
+    end
+
+    it 'uses the schema with the highest priority' do
+      transformer = Arel::Transformer::PrefixSchemaName.new(
+        connection,
+        %w[priority normal],
+        'posts' => %w[normal priority],
+        'comments' => ['normal'],
+      )
+      sql = 'SELECT * FROM "posts", "comments"'
+      arel = Arel.sql_to_arel(sql)
+      prefixed_sql = transformer.call(arel.first, nil).to_sql
+
+      expect(prefixed_sql).to eq 'SELECT * FROM "priority"."posts", "normal"."comments"'
+    end
+
+    it 'does not prefix a table in the pg_catalog namespace' do
+      transformer = Arel::Transformer::PrefixSchemaName.new(connection)
+      sql = 'SELECT * FROM "pg_class"'
+      arel = Arel.sql_to_arel(sql)
+      prefixed_sql = transformer.call(arel.first, nil).to_sql
+
+      expect(prefixed_sql).to eq sql
+    end
+
+    it 'fails for an unknown table' do
+      transformer = Arel::Transformer::PrefixSchemaName.new(connection)
+      sql = 'SELECT 1 FROM unknown_table'
+      arel = Arel.sql_to_arel(sql)
+
+      expect do
+        transformer.call(arel.first, nil).to_sql
+      end.to raise_error('Table `unknown_table` is an unknown table and cannot be prefixed')
+    end
+  end
+
+  context 'regclass' do
+    it 'prefixes a string casted as a regclass' do
+      transformer = Arel::Transformer::PrefixSchemaName.new(connection)
+      sql = "SELECT 'posts'::regclass"
+      arel = Arel.sql_to_arel(sql)
+      prefixed_sql = transformer.call(arel.first, nil).to_sql
+
+      expect(prefixed_sql).to eq "SELECT 'public.posts'::regclass"
+    end
+
+    it 'does not update a string which already has a schema' do
+      transformer = Arel::Transformer::PrefixSchemaName.new(connection)
+      sql = "SELECT 'secret.posts'::regclass"
+      arel = Arel.sql_to_arel(sql)
+      prefixed_sql = transformer.call(arel.first, nil).to_sql
+
+      expect(prefixed_sql).to eq "SELECT 'secret.posts'::regclass"
+    end
+
+    it 'raises when the regclass consists of three or more parts' do
+      transformer = Arel::Transformer::PrefixSchemaName.new(connection)
+      sql = "SELECT 'something.secret.posts'::regclass"
+      arel = Arel.sql_to_arel(sql)
+
+      expect do
+        transformer.call(arel.first, nil).to_sql
+      end.to raise_error(/Don't know how to handle `3` parts in/)
+    end
+  end
+end
