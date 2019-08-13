4ee9c5a271e48e2b3aa41c558f0f3c5f9a5b4d8f
maarten <maarten@vgijssel.nl>
2019-08-13 17:14:28 +0200

    Get full SQL from a block without executing the SQL

--- lib/arel/middleware.rb
+++ lib/arel/middleware.rb
@@ -1,7 +1,9 @@
 require 'active_record'
 require_relative './middleware/railtie'
 require_relative './middleware/chain'
-require_relative './middleware/executor'
+require_relative './middleware/database_executor'
+require_relative './middleware/to_sql_executor'
+require_relative './middleware/to_sql_middleware'
 require_relative './middleware/result'
 require_relative './middleware/postgresql_adapter'
 
---
+++ lib/arel/middleware/to_sql_executor.rb
@@ -0,0 +1,15 @@
+module Arel
+  module Middleware
+    class ToSqlExecutor < DatabaseExecutor
+      private
+
+      def execute_sql(next_arel)
+        Arel::Middleware::Result.create(
+          data: next_arel.to_sql,
+          from: Arel::Middleware::StringResult,
+          to: Arel::Middleware::EmptyPGResult,
+        )
+      end
+    end
+  end
+end
