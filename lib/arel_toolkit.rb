54076c5d42808cbc4c3912cad687a9fd6ffa4655
maarten <maarten@vgijssel.nl>
2019-08-15 16:43:08 +0200

    random stuff

--- lib/arel_toolkit.rb
+++ lib/arel_toolkit.rb
@@ -17,6 +17,10 @@ end
 
 require 'ffi'
 
+# What we want to do here:
+# instantiate a PGresult struct in C
+# somehow get a VALUE object for pg_conn
+# instantiate a PG::Result object in ruby
 module Test
   extend FFI::Library
 
@@ -25,4 +29,36 @@ module Test
   path = File.join(dir, file)
 
   ffi_lib path
+
+  typedef :pointer, :pg_result
+  # Lists all the type definitions available: FFI::TypeDefs
+  # This is wrong, it's not a pointer but a VALUE object
+  typedef :pointer, :pg_conn
+
+  # This is probably the method to instantiate a PG::Result
+  # From the pg gem
+  attach_function :pg_new_result, [:pg_result, :pg_conn], :void
+
+  # the definition of the PGresult or pg_result Struct in postgres/postgres
+  # src/interfaces/libpq/libpq-int.h:167
+  class PGData < FFI::Struct
+    layout :ntups, :int,
+           :numAttributes, :int
+  end
+
+  data = PGData.new
+  data[:ntups] = 0
+  data[:numAttributes] = 0
+  data_pointer = data.pointer
+
+  # Ruby object to C pointer and back to Ruby object using fiddle
+  # https://stackoverflow.com/questions/41516431/can-i-pass-a-ruby-object-pointer-to-a-ruby-ffi-callback
+
+
+  # pg_new_result(data_pointer, data_pointer)
+
+  # get the PG connection
+  # conn = ActiveRecord::Base.connection.raw_connection
+
+  # attach_function :count, [:int, :float], :double
 end
