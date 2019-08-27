303fec1ef204a4f511fe205397c5fab63923bd90
maarten <maarten@vgijssel.nl>
2019-08-27 20:26:15 +0200

    Getting the pg result attribute description to work

--- lib/arel_toolkit.rb
+++ lib/arel_toolkit.rb
@@ -32,29 +32,99 @@ module Test
   # https://medium.com/@astantona/fiddling-with-rubys-fiddle-39f991dd0565
   extend FFI::Library
 
-  # # https://stackoverflow.com/questions/41516431/can-i-pass-a-ruby-object-pointer-to-a-ruby-ffi-callback
-  # def self.pointer_to_ruby(ffi_pointer)
-  #   require 'fiddle'
-  #   address = ffi_pointer.to_i
-  #   fiddle = ::Fiddle::Pointer.new(address)
-  #   fiddle.to_value
-  # end
-
   dir = Gem.loaded_specs.fetch('pg').stub.extension_dir
   file = 'pg_ext.bundle'
   path = File.join(dir, file)
 
   ffi_lib path
 
-  typedef :ulong, :self # <<-- look, just unsigned long!
+  typedef :ulong, :self
   typedef :pointer, :pg_result
 
   attach_function :pgresult_get, [:self], :pg_result
 
+  # https://www.postgresql.org/docs/10/libpq-misc.html
+  typedef :string, :value
+  attach_function :pq_set_value, :PQsetvalue, [:pg_result, :int, :int, :value, :int], :int
+
+  # Maybe use PQcopyResult to make a copy of an existing result object
+  # pass proper flags to not copy the attributes and tuples
+  # next set the attributes and tuples
+  def self.test2
+    result = ActiveRecord::Base.connection.raw_connection.make_empty_pgresult(2)
+    result_pointer = Test.ruby_to_pointer(result)
+    pg_result_pointer = Test.pgresult_get(result_pointer)
+    Test.pq_set_value(pg_result_pointer, 0, 0, 'papi', 0)
+  end
+
+  typedef :pointer, :att_descs
+  attach_function :pq_set_result_attrs, :PQsetResultAttrs, [:pg_result, :int, :att_descs], :int
+
+  # src/include/postgres_ext.h:31
+  # typedef unsigned int Oid;
+  typedef :uint, :oid
+
+  # src/interfaces/libpq/libpq-fe.h:235
+  class PGresAttDesc < FFI::Struct
+    layout :name, :string,
+           :tableid, :oid,
+           :columnid, :int,
+           :format, :int,
+           :typid, :oid,
+           :typlen, :int,
+           :atttypmod, :int
+
+    def to_h
+      result = {}
+
+      members.each do |member|
+        result[member] = self[member]
+      end
+
+      result
+    end
+  end
+
+  # For query `SELECT 1 as kerk`
+  # name => 'kerk'
+  # tableid => 0
+  # columnid => 0
+  # format => 0
+  # typid => 23
+  # typlen => 4
+
+  # src/interfaces/libpq/libpq-int.h:167
   class PGData < FFI::Struct
     layout :ntups, :int,
-           :numAttributes, :int
+           :numAttributes, :int,
+           :attDescs, :pointer
+
+    def attributes
+      val_array = FFI::Pointer.new(Test::PGresAttDesc, self[:attDescs])
+
+      0.upto(self[:numAttributes] - 1).map do |i|
+        Test::PGresAttDesc.new(val_array[i])
+      end
+    end
   end
+
+  def self.test3
+    # result = ActiveRecord::Base.connection.raw_connection.make_empty_pgresult(2)
+    result = ActiveRecord::Base.connection.execute("SELECT 1 AS kerk, 'hello' AS shine")
+    result_pointer = Test.ruby_to_pointer(result)
+    pg_result_pointer = Test.pgresult_get(result_pointer)
+    r = Test::PGData.new(pg_result_pointer)
+    # att_desc = Test::PGresAttDesc.new
+    # att_desc[:name] = 'test'
+    # Test.pq_set_result_attrs(pg_result_pointer, 1, nil)
+  end
+
+  # Instead of duplicating the struct here, modify the struct using public methods
+  # https://www.postgresql.org/docs/10/libpq-misc.html
+  # PQsetResultAttrs
+  # PQsetvalue
+  # PQmakeEmptyPGresult modifies all the necessary attributes
+  # src/interfaces/libpq/fe-exec.c:142
 
   # https://stackoverflow.com/questions/41516431/can-i-pass-a-ruby-object-pointer-to-a-ruby-ffi-callback
   def self.ruby_to_pointer(obj)
@@ -65,7 +135,7 @@ module Test
     # ffi_pointer = ::FFI::Pointer.new(:pointer, address)
   end
 
-  def self.do_it
+  def self.test1
     # result = ActiveRecord::Base.connection.raw_connection.make_empty_pgresult(2)
     result = ActiveRecord::Base.connection.execute('SELECT 1')
     result_pointer = Test.ruby_to_pointer(result)
@@ -74,7 +144,9 @@ module Test
 
     puts result.ntuples
 
-    pg_data[:ntups] = 10
+    binding.pry
+
+    # pg_data[:ntups] = 10
 
     puts result.ntuples
   end
