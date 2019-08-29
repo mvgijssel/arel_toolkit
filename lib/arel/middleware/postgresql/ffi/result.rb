d1d539ebd0c0abed4211fd7354855ed0424e44e5
maarten <maarten@vgijssel.nl>
2019-08-29 17:20:14 +0200

    Basic working version of multi dimensional array of structs

--- lib/arel/middleware/postgresql/ffi/result.rb
+++ lib/arel/middleware/postgresql/ffi/result.rb
@@ -9,23 +9,9 @@ module Arel
           attribute :attDescs, :pointer,
                     caster: ::FFI::StructArrayCaster.new(Postgresql::FFI::Column, :num_attributes),
                     as: :attributes
-          attribute :tuples, :pointer, as: :rows
-
-          # # https://zegoggl.es/2009/05/ruby-ffi-recipes.html
-          # # tuples are stored in a multi dimensional array, pointers of pointers
-          # def values
-          #   tuple_pointers = ::FFI::Pointer.new(:pointer, self[:tuples])
-
-          #   0.upto(self[:ntups] - 1).map do |tuple_index|
-          #     tuple_pointer = tuple_pointers[tuple_index].read_pointer
-
-          #     column_pointer = ::FFI::Pointer.new(Postgresql::FFI::PGresAttValue, tuple_pointer)
-
-          #     0.upto(self[:numAttributes] - 1).map do |column_index|
-          #       Postgresql::FFI::PGresAttValue.new(column_pointer[column_index])
-          #     end
-          #   end
-          # end
+          attribute :tuples, :pointer,
+                    caster: ::FFI::StructMultiArrayCaster.new(Postgresql::FFI::Value, :num_rows, :num_attributes),
+                    as: :rows
         end
       end
     end
