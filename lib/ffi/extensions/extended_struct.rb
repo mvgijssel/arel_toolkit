154865d4092dd725a5a8d14d91be1401a78fa99c
maarten <maarten@vgijssel.nl>
2019-09-02 16:36:21 +0200

    Make extended struct inherit from regular struct

--- lib/ffi/extensions/extended_struct.rb
+++ lib/ffi/extensions/extended_struct.rb
@@ -1,5 +1,5 @@
 module FFI
-  class ExtendedStruct
+  class ExtendedStruct < FFI::Struct
     def self.attribute(name, type, caster: NativeCaster, as: nil)
       external_name = as || name
       internal_name = name
@@ -28,24 +28,15 @@ module FFI
       }
     end
 
-    def self.enclosing_module
-      @enclosing_module ||= begin
-                              ref = self
-
-                              wrapper = Class.new(FFI::Struct) do
-                                define_singleton_method(:name) do
-                                  ref.name
-                                end
-                              end
-
-                              wrapper.send(:enclosing_module)
-                            end
-    end
-
     def self.from_pointer(pointer)
       new(pointer, {})
     end
 
+    def self.from_data(**kwargs)
+      new(nil, kwargs)
+    end
+
+    # TODO: remove this
     def self.to_array_pointer(items)
       # Create a memory pointer to hold all of the array items
       array_pointer = ::FFI::MemoryPointer.new(size, items.length)
@@ -53,42 +44,49 @@ module FFI
       # Read the bytes of each struct and store these bytes in
       # the right place in the memory pointer
       items.each_with_index do |item, index|
-        item_bytes = item.internal.pointer.get_bytes(0, size)
+        item_bytes = item.pointer.get_bytes(0, size)
         array_pointer[index].put_bytes(0, item_bytes)
       end
 
       array_pointer
     end
 
-    def self.from_data(**kwargs)
-      new(nil, kwargs)
-    end
-
     def self.attributes
       @attributes
     end
 
-    def self.internal_class
-      ref = self
+    # def self.internal_class
+    #   ref = self
 
-      @internal_class ||= Class.new(FFI::Struct) do
-        ref_layout = ref.attributes.map do |_name, value|
-          [value.fetch(:internal_name), value.fetch(:type)]
-        end.flatten
+    #   @internal_class ||= Class.new(FFI::Struct) do
+    #     ref_layout = ref.attributes.map do |_name, value|
+    #       [value.fetch(:internal_name), value.fetch(:type)]
+    #     end.flatten
 
-        layout(*ref_layout)
-      end
+    #     layout(*ref_layout)
+    #   end
+    # end
+
+    # def self.size
+    #   internal_class.size
+    # end
+
+    def self.ensure_layout
+      return if @layout
+
+      ref_layout = attributes.map do |_name, value|
+        [value.fetch(:internal_name), value.fetch(:type)]
+      end.flatten
+
+      layout(*ref_layout)
     end
-
-    def self.size
-      internal_class.size
-    end
-
-    attr_reader :internal
 
     def initialize(pointer = nil, **kwargs)
       @remembered_pointers = {}
-      @internal = self.class.internal_class.new(pointer)
+
+      self.class.ensure_layout
+
+      super(pointer)
 
       self.class.attributes.each do |name, _value|
         public_send "#{name}=", kwargs.fetch(name, public_send(name))
