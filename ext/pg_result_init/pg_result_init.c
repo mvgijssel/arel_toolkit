702159a96eacea6a7bd3e6130ac56b5a56d1ce29
maarten <maarten@vgijssel.nl>
2019-09-05 14:57:17 +0200

    Added the rest of the column attributes

--- ext/pg_result_init/pg_result_init.c
+++ ext/pg_result_init/pg_result_init.c
@@ -36,26 +36,37 @@ pg_result_init_create(VALUE self, VALUE rb_pgconn, VALUE rb_columns, VALUE rb_ro
   int index;
   VALUE rb_column;
   VALUE rb_column_name;
+  VALUE rb_table_id;
+  VALUE rb_column_id;
+  VALUE rb_format;
+  VALUE rb_typ_id;
+  VALUE rb_typ_len;
+  VALUE rb_att_typemod;
+
   char *column_name;
 
   for(index = 0; index < num_columns; index++) {
     rb_column = rb_ary_entry(rb_columns, index);
     rb_column_name = rb_funcall(rb_column, rb_intern("fetch"), 1, ID2SYM(rb_intern("name")));
+    rb_table_id = rb_funcall(rb_column, rb_intern("fetch"), 2, ID2SYM(rb_intern("tableid")), INT2NUM(0));
+    rb_column_id = rb_funcall(rb_column, rb_intern("fetch"), 2, ID2SYM(rb_intern("columnid")), INT2NUM(0));
+    rb_format = rb_funcall(rb_column, rb_intern("fetch"), 2, ID2SYM(rb_intern("format")), INT2NUM(0));
+    rb_typ_id = rb_funcall(rb_column, rb_intern("fetch"), 2, ID2SYM(rb_intern("typid")), INT2NUM(0));
+    rb_typ_len = rb_funcall(rb_column, rb_intern("fetch"), 2, ID2SYM(rb_intern("typlen")), INT2NUM(0));
+    rb_att_typemod = rb_funcall(rb_column, rb_intern("fetch"), 2, ID2SYM(rb_intern("atttypmod")), INT2NUM(-1));
 
     // Using StringValueCStr, if column contains null bytes it should raise Argument error
     // postgres does not handle null bytes.
     column_name = StringValueCStr(rb_column_name);
 
-    rb_p(rb_column_name);
-
     // postgres/src/interfaces/libpq/libpq-fe.h:235
     attDescs[index].name = column_name; // TODO: do we need to copy the contents or can we use a reference?
-    attDescs[index].tableid = 0;
-    attDescs[index].columnid = 0;
-    attDescs[index].format = 0; // 0 is text, 1 is binary https://www.postgresql.org/docs/10/libpq-exec.html
-    attDescs[index].typid = 0;
-    attDescs[index].typlen = 4;
-    attDescs[index].atttypmod = -1; // -1 no type modifier
+    attDescs[index].tableid = NUM2INT(rb_table_id); // TODO: tableid is Oid type, not integer
+    attDescs[index].columnid = NUM2INT(rb_column_id);
+    attDescs[index].format = NUM2INT(rb_format); // 0 is text, 1 is binary https://www.postgresql.org/docs/10/libpq-exec.html
+    attDescs[index].typid = NUM2INT(rb_typ_id); // TODO: typid is Oid type, not integer
+    attDescs[index].typlen = NUM2INT(rb_typ_len);
+    attDescs[index].atttypmod = NUM2INT(rb_att_typemod); // -1 no type modifier
   }
 
   PQsetResultAttrs(result, num_columns, attDescs);
