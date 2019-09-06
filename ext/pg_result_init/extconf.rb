c506256a9f85f81990f8f6e1ab3f4275eae437eb
maarten <maarten@vgijssel.nl>
2019-09-06 13:52:21 +0200

    Maybe fix now

--- ext/pg_result_init/extconf.rb
+++ ext/pg_result_init/extconf.rb
@@ -1,26 +1,5 @@
-# read, write = IO.pipe
-
-# pid = Process.fork do
-#   require 'pg'
-#   pg_ext = Gem.loaded_specs.fetch('pg')
-#   pg_ext_inlude_dir = File.join(pg_ext.full_gem_path, 'ext')
-#   pg_ext_lib_dir = pg_ext.extension_dir
-#   write.puts("#{pg_ext_inlude_dir},#{pg_ext_lib_dir}")
-#   puts "DONE PROCESS"
-# end
-
-# Process.wait(pid)
-# write.close
-# pg_ext_include_dir, pg_ext_lib_dir = read.read.chomp.split(",")
-# read.close
-
-# require 'pry'
-# binding.pry
-
-pg_ext_include_dir = "/Users/maarten/.anyenv/envs/rbenv/versions/2.5.3/lib/ruby/gems/2.5.0/gems/pg-1.1.4/ext"
-pg_ext_lib_dir = '/Users/maarten/.anyenv/envs/rbenv/versions/2.5.3/lib/ruby/gems/2.5.0/extensions/x86_64-darwin-18/2.5.0-static/pg-1.1.4'
-
 require 'mkmf'
+require 'pg'
 
 CONFIG['debugflags'] = '-ggdb3'
 CONFIG['optflags'] = '-O0'
@@ -46,12 +25,13 @@ dir_config(
   pg_lib_dir,
 )
 
-# require 'pry'
-# binding.pry
+pg_ext = Gem.loaded_specs.fetch('pg')
+pg_ext_inlude_dir = File.join(pg_ext.full_gem_path, 'ext')
+pg_ext_lib_dir = pg_ext.extension_dir
 
 dir_config(
   'pg_ext',
-  pg_ext_include_dir,
+  pg_ext_inlude_dir,
   pg_ext_lib_dir,
 )
 
@@ -65,10 +45,9 @@ if (
 
   # have_func 'PQsetSingleRowMode'
   # have_func 'timegm'
-
   # TODO: check pg_result_init.c for required enums / functions
 
   create_makefile('arel_toolkit/pg_result_init')
 else
-  abort 'Could not find PostgreSQL build environment (libraries & headers): Makefile not created'
+  puts 'Could not find PostgreSQL build environment (libraries & headers): Makefile not created'
 end
