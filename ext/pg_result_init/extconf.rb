e0c242b37720ccf7008e11940a60c2006048173f
maarten <maarten@vgijssel.nl>
2019-09-06 10:21:25 +0200

    Fix setup extension

--- ext/pg_result_init/extconf.rb
+++ ext/pg_result_init/extconf.rb
@@ -1,4 +1,5 @@
 require 'mkmf'
+require 'pg'
 
 CONFIG['debugflags'] = '-ggdb3'
 CONFIG['optflags'] = '-O0'
@@ -24,25 +25,23 @@ dir_config(
   pg_lib_dir,
 )
 
-# puts Gem.loaded_specs
+pg_ext = Gem.loaded_specs.fetch('pg')
+pg_ext_inlude_dir = File.join(pg_ext.full_gem_path, 'ext')
+pg_ext_lib_dir = pg_ext.extension_dir
 
-# pg_ext = Gem.loaded_specs.fetch('pg').stub
-# pg_ext_inlude_dir = File.join(pg_ext.full_gem_path, 'ext')
-# pg_ext_lib_dir = pg_ext.extension_dir
-
-# dir_config(
-#   'pg_ext',
-#   pg_ext_inlude_dir,
-#   pg_ext_lib_dir,
-# )
+dir_config(
+  'pg_ext',
+  pg_ext_inlude_dir,
+  pg_ext_lib_dir,
+)
 
 if (
     have_library('pq') ||
     have_library('libpq') ||
     have_library('ms/libpq')
   ) &&
-   have_header('libpq-fe.h')
-   # && have_header('pg.h')
+   have_header('libpq-fe.h') &&
+   have_header('pg.h')
 
   # have_func 'PQsetSingleRowMode'
   # have_func 'timegm'
