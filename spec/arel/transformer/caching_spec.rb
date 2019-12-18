44e960675b64ec7c0588ecb6d73dfff803281625
Willian van der Velde <willian@hackerone.com>
2019-12-18 15:00:13 +0100

    Wrapped middleware

--- spec/arel/transformer/caching_spec.rb
+++ spec/arel/transformer/caching_spec.rb
@@ -1,19 +1,30 @@
 describe 'Middleware Query Caching' do
   let(:next_middleware) { ->(new_arel) { new_arel } }
 
+  class MiddlewareOne
+    def call(arel, next_middleware)
+      next_middleware.call arel
+    end
+  end
+
+  class MiddlewareTwo
+    def call(arel, next_middleware)
+      next_middleware.call arel
+    end
+  end
+
   it 'stores the result of the middleware transformation' do
     Post.first # Warm up cache
 
-    transformer = Class.new do
-      def call(arel, next_middleware)
-        next_middleware.call arel
-      end
-    end.new
+    middleware_one = MiddlewareOne.new
+    middleware_two = MiddlewareTwo.new
 
     cache = spy('cache', get: nil, set: nil)
 
-    Arel.middleware.apply([], cache: cache) do
-      Arel.middleware.apply([transformer]) { Post.first }
+    Arel.middleware.apply([middleware_one], cache: cache) do
+      Arel.middleware.apply([middleware_two]) do
+        Post.first
+      end
     end
 
     expect(cache).to \
