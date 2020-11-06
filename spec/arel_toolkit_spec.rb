cfd36d78b7a0ec665396eb8f7d92aac8f6bb0f9f
Alexandra Khegay <khaleksa@gmail.com>
2020-11-06 15:56:39 +0500

    add spec for arel gem version

--- spec/arel_toolkit_spec.rb
+++ spec/arel_toolkit_spec.rb
@@ -2,4 +2,9 @@ describe ArelToolkit do
   it 'has a version number' do
     expect(ArelToolkit::VERSION).not_to be nil
   end
+
+  it 'has version of Arel corresponding to ActiveRecord' do
+    arel_version = ActiveRecord.version.version.to_i == 6 ? 10 : 9
+    expect(Arel::VERSION.to_i).to be arel_version
+  end
 end
