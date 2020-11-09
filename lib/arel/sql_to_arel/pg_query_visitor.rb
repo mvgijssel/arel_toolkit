02da793d30770be4f00a69cf219e8bf12dd19bfc
Alexandra Khegay <khaleksa@gmail.com>
2020-11-09 16:13:14 +0500

    define Arel::Nodes::Top

--- lib/arel/sql_to_arel/pg_query_visitor.rb
+++ lib/arel/sql_to_arel/pg_query_visitor.rb
@@ -719,9 +719,7 @@ module Arel
         select_core.havings = [visit(having_clause)] if having_clause
         select_core.windows = visit(window_clause) if window_clause
         select_core.into = visit(into_clause) if into_clause
-        if (ActiveRecord.version.version.to_i != 6) && limit_count
-          select_core.top = ::Arel::Nodes::Top.new visit(limit_count)
-        end
+        select_core.top = ::Arel::Nodes::Top.new visit(limit_count) if limit_count
 
         if distinct_clause == [nil]
           select_core.set_quantifier = Arel::Nodes::Distinct.new
