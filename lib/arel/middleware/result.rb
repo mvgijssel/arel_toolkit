59da897f3bde49079b03a4bb2b9916591bd63ef8
Brian Reigersberg <brian@hackerone.com>
2022-01-10 10:05:01 +0100

    Revert "temp delegate"
    This reverts commit 509503557df63cd3db35f4e1f00529844fac5ae6.

--- lib/arel/middleware/result.rb
+++ lib/arel/middleware/result.rb
@@ -14,7 +14,6 @@ module Arel
     # activerecord/lib/active_record/result.rb
     class Result
       attr_reader :original_data
-      delegate :fields, :clear, :ftype, :fmod, :values, to: :@original_data
 
       def self.create(from:, to:, data:)
         Result.new from, to, data
