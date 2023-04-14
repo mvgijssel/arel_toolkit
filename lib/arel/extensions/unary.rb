c66f58b5ceef64c65cdae0f32a447dbd3fc4a2ee
Jos Ahrens <jos@hackerone.com>
2023-04-14 14:03:37 +0200

    remove bridges for activerecord 6 or lower

--- lib/arel/extensions/unary.rb
+++
@@ -1,7 +0,0 @@
-module Arel
-  module Visitors
-    class Dot
-      alias visit_Arel_Nodes_Unary unary if ActiveRecord::VERSION::MAJOR < 7
-    end
-  end
-end
--- spec/arel_toolkit_spec.rb
+++ spec/arel_toolkit_spec.rb
@@ -4,7 +4,6 @@ describe ArelToolkit do
   end
 
   it 'has version of Arel corresponding to ActiveRecord' do
-    arel_version = ActiveRecord.version.version.to_i >= 6 ? 10 : 9
-    expect(Arel::VERSION.to_i).to be arel_version
+    expect(Arel::VERSION.to_i).to be 10
   end
 end
